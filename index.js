dd1fda11aa2187d90100d54733a9c605952d86ce
Nils Schönwald <nils@schoenwald.media>
2016-07-27 02:31:37 +0200

    Update 2016-07-27T00:31:35.974Z

--- index.js
+++ index.js
@@ -1276,14 +1276,19 @@ var Stargate = function (_HTMLElement) {
           address: [27, 7, 15, 32, 12, 30, 1],
           speed: 4000, // should pe same as css-transition '.Stargate-symboles'
           delay: 2000,
+          lockTime: 600,
           currentIndex: 0,
           isRunning: false,
+          hooks: [1, 2, 3, 6, 7, 8, 0],
           selectors: {
             symboles: '.Stargate-symboles',
             symbole: '.Stargate-symbole',
             eventHorizon: '.Stargate-horizon',
+            chevrons: '.Stargate-chevron',
+            video: '.Stargate-video',
             buttonGo: '.Stargate-button--go',
-            buttonStop: '.Stargate-button--stop'
+            buttonStop: '.Stargate-button--stop',
+            buttonTC: '.Stargate-button--toggleChevrons'
           }
         };
 
@@ -1299,6 +1304,9 @@ var Stargate = function (_HTMLElement) {
         this.elements.buttonStop[0].addEventListener('click', function () {
           return _this2.removeAttribute('data-address');
         });
+        this.elements.buttonTC[0].addEventListener('click', function () {
+          return _this2.toggleChevrons();
+        });
       }
 
       return createdCallback;
@@ -1309,6 +1317,7 @@ var Stargate = function (_HTMLElement) {
       function attributeChangedCallback(name, oldValue, newValue) {
         if (newValue != null) {
           this.options.address = JSON.parse(newValue);
+          this.resetGate();
           this.runGate();
         } else {
           this.options.address = [];
@@ -1326,6 +1335,9 @@ var Stargate = function (_HTMLElement) {
         this.options.currentIndex = 0;
         this.options.isRunning = false;
         this.setHorizon(false);
+        this.elements.chevrons.forEach(function (element) {
+          return element.classList.remove('is-active');
+        });
       }
 
       return resetGate;
@@ -1338,7 +1350,7 @@ var Stargate = function (_HTMLElement) {
           next: function () {
             function next(that, index, address) {
               if (index < address.length) {
-                that.rotateTo(that.options.address[index]);
+                that.rotateTo(index);
                 that.increaseIndex();
                 return { done: false };
               }
@@ -1368,8 +1380,11 @@ var Stargate = function (_HTMLElement) {
       function setHorizon(state) {
         if (state === true && this.options.isRunning) {
           this.elements.eventHorizon[0].classList.add('is-active');
+          this.elements.video[0].play();
         } else {
           this.elements.eventHorizon[0].classList.remove('is-active');
+          this.elements.video[0].pause();
+          this.elements.video[0].load();
         }
       }
 
@@ -1388,11 +1403,16 @@ var Stargate = function (_HTMLElement) {
   }, {
     key: 'rotateTo',
     value: function () {
-      function rotateTo(chevron) {
+      function rotateTo(index) {
         var _this3 = this;
 
+        var chevron = this.options.address[index];
         console.log('Rotating to ' + chevron);
         this.elements.symboles[0].style.transform = 'rotate(' + 360 / 39 * (chevron - 1) * -1 + 'deg)';
+
+        setTimeout(function () {
+          return _this3.activateHook(_this3.options.hooks[index]);
+        }, this.options.speed);
 
         setTimeout(function () {
           return _this3.rotate().next(_this3, _this3.options.currentIndex, _this3.options.address);
@@ -1404,11 +1424,41 @@ var Stargate = function (_HTMLElement) {
   }, {
     key: 'activateHook',
     value: function () {
-      function activateHook(hook) {
-        console.log(hook);
+      function activateHook(index) {
+        var _this4 = this;
+
+        this.lockChevron();
+        setTimeout(function () {
+          return _this4.elements.chevrons[index].classList.add('is-active');
+        }, this.options.lockTime);
       }
 
       return activateHook;
+    }()
+  }, {
+    key: 'lockChevron',
+    value: function () {
+      function lockChevron() {
+        var _this5 = this;
+
+        this.elements.chevrons[0].classList.add('is-open');
+        setTimeout(function () {
+          return _this5.elements.chevrons[0].classList.remove('is-open');
+        }, this.options.lockTime / 2);
+      }
+
+      return lockChevron;
+    }()
+  }, {
+    key: 'toggleChevrons',
+    value: function () {
+      function toggleChevrons() {
+        this.elements.chevrons.forEach(function (element) {
+          return element.classList.toggle('is-active');
+        });
+      }
+
+      return toggleChevrons;
     }()
   }], [{
     key: 'observedAttributes',
